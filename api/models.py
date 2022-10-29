48ce9f2b88f208d0047f19c7488d648fd0de289e
ZM <zanamziry720@gmail.com>
2022-10-29 21:52:12 +0300

    idk

--- api/models.py
+++
@@ -1,9 +0,0 @@
-from enum import unique
-from django.db import models
-
-# Create your models here.
-class Agents(models.Model):
-    id = models.CharField(max_length=10, primary_key=True)
-    username = models.CharField(max_length=64)
-    password = models.CharField(max_length=64)
-    
