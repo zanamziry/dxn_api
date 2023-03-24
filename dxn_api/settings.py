cf00d00a885f04900e5e6c2641c2a62bcf1e5dc7
ZM <zanamziry720@gmail.com>
2023-03-24 13:07:09 +0300

    add build.sh for deployment on render added poetry

--- dxn_api/settings.py
+++ dxn_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import rest_framework
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m4=uae13#1az0vn#10tvga)y)2_if(=9q6x(y6o#3dad#)!971'
+#django-insecure-m4=uae13#1az0vn#10tvga)y)2_if(=9q6x(y6o#3dad#)!971
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['dxn-api.onrender.com', '127.0.0.1','http://zanamziry.pythonanywhere.com' ]
+ALLOWED_HOSTS = ['127.0.0.1', 'zanamziry.pythonanywhere.com' ]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
