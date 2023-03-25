c3463f06a6ed976cd02bca875004b40cc0951f4b
ZM <zanamziry720@gmail.com>
2023-03-26 00:12:17 +0300

    s

--- dxn_api/settings.py
+++ dxn_api/settings.py
@@ -135,8 +135,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
