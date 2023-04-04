08d4fdbf6d4f2a4cff23f142af26fab0c1119db0
ZM <zanamziry720@gmail.com>
2023-04-05 01:58:22 +0300

    envirement variable

--- dxn_api/settings.py
+++ dxn_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #django-insecure-m4=uae13#1az0vn#10tvga)y)2_if(=9q6x(y6o#3dad#)!971
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
