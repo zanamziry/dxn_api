f0c0e3f23abd264d0717cf49da2513a554aca2c6
ZM <zanamziry720@gmail.com>
2023-04-06 01:13:49 +0300

    test

--- dxn_api/wsgi.py
+++ dxn_api/wsgi.py
@@ -8,11 +8,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
-project_folder = os.path.expanduser('~/dxn_api')
-load_dotenv(os.path.join(project_folder,'.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dxn_api.settings')
 
 application = get_wsgi_application()
