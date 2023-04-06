b31048a0557fe236c1dfccc2dcac40c65b386bb0
ZM <zanamziry720@gmail.com>
2023-04-06 20:39:36 +0300

    ok

--- dxn_api/wsgi.py
+++ dxn_api/wsgi.py
@@ -8,13 +8,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-from pathlib import Path
-from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
-'''
-BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
-'''
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dxn_api.settings')
 
 application = get_wsgi_application()
